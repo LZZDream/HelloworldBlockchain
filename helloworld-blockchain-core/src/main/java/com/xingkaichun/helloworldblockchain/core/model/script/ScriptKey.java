f80c71806d7a1ab281e9b7ae60a0588abb0c241d
邢开春 <xingkaichun@qq.com>
2020-11-09 14:00:12 +0800

    2020110901

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/script/ScriptKey.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/script/ScriptKey.java
@@ -2,7 +2,7 @@ package com.xingkaichun.helloworldblockchain.core.model.script;
 
 /**
  * 脚本钥匙
- *
+ * //TODO Input Script
  * @author 邢开春 微信HelloworldBlockchain 邮箱xingkaichun@qq.com
  */
 public class ScriptKey extends Script {
--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/script/ScriptLock.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/model/script/ScriptLock.java
@@ -2,7 +2,7 @@ package com.xingkaichun.helloworldblockchain.core.model.script;
 
 /**
  * 脚本锁
- *
+ * //TODO Output Script
  * @author 邢开春 微信HelloworldBlockchain 邮箱xingkaichun@qq.com
  */
 public class ScriptLock extends Script {
