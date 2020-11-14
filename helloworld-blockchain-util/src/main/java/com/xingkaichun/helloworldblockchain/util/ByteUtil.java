6a362ac63080c4374860fdd1914606f167986f93
邢开春 <xingkaichun@ceair.com>
2020-11-14 10:38:51 +0800

    2020111404

--- helloworld-blockchain-util/src/main/java/com/xingkaichun/helloworldblockchain/util/ByteUtil.java
+++ helloworld-blockchain-util/src/main/java/com/xingkaichun/helloworldblockchain/util/ByteUtil.java
@@ -28,32 +28,17 @@ public class ByteUtil {
     }
 
     /**
-     * int转换为4个字节的字节数组(4*8=32个bit)。
-     * //TODO
-     */
-    private static byte[] intToBytes4(int value) {
-        byte[] bytes = new byte[4];
-        bytes[3] = (byte)(0xFF & (value));
-        bytes[2] = (byte)(0xFF & (value >> 8));
-        bytes[1] = (byte)(0xFF & (value >> 16));
-        bytes[0] = (byte)(0xFF & (value >> 24));
-        return bytes;
-    }
-
-
-
-    /**
      * 拼接字节数组。计算[传入字节数组]的长度，然后将长度转为4个字节的字节数组(大端)，然后将长度字节数组拼接在[传入字节数组]前，然后返回。
      */
     public static byte[] concatLengthBytes(byte[] value) {
-        return Bytes.concat(intToBytes4(value.length),value);
+        return Bytes.concat(longToBytes8(value.length),value);
     }
 
     /**
      * 拼接字节数组。
      */
     public static byte[] concatLengthBytes(List<byte[]> values) {
-        byte[] concatBytes = intToBytes4(values.size());
+        byte[] concatBytes = longToBytes8(values.size());
         for(byte[] value:values){
             concatBytes = Bytes.concat(concatBytes,concatLengthBytes(value));
         }
